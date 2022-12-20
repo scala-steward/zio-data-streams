8acfdf5db6af9c879946c06196106c5940dfee5a
Gregor Purdy <gregor@abcelo.com>
2022-12-19 19:32:16 -0800

    Accept reformats

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val zioVersion = "2.0.4"
 
-  lazy val zioStreams = "dev.zio" %% "zio-streams" % zioVersion
-  lazy val zioTest = "dev.zio" %% "zio-test" % zioVersion
+  lazy val zioStreams = "dev.zio" %% "zio-streams"  % zioVersion
+  lazy val zioTest    = "dev.zio" %% "zio-test"     % zioVersion
   lazy val zioTestSbt = "dev.zio" %% "zio-test-sbt" % zioVersion
 }
