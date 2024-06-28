8c1dbc6fa0469d95309a19bc5bdb3b382bace1c5
Scala Steward <scala_steward@virtuslab.com>
2024-06-28 16:18:12 +0000

    Update zio-streams, zio-test, ... to 2.1.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val zioVersion = "2.1.1"
+  lazy val zioVersion = "2.1.5"
 
   // lazy val zioStreams = "dev.zio" %%% "zio-streams"  % zioVersion
   // lazy val zioTest    = "dev.zio" %%% "zio-test"     % zioVersion
